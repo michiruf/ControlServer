98e368c1d3667c7e2305a9305e6a54549468ade2
Michael Ruf <Ruf_Michael@gmx.de>
2015-09-28 23:46:10 +0200

    Fix provided configuration using a plugin; Use junit 4 syntax instad of 3

--- server/src/test/java/de/michiruf/control_server/PlaygroundTest.java
+++ server/src/test/java/de/michiruf/control_server/PlaygroundTest.java
@@ -1,19 +1,26 @@
 package de.michiruf.control_server;
 
-import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Michael Ruf
  * @since 2015-09-08
  */
-public class PlaygroundTest extends TestCase {
+public class PlaygroundTest {
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
+    private TestClient client;
+
+    @SuppressWarnings("unused")
+    @Before
+    public void setUp() {
         ControlServer.main(new String[0]);
+        client = new TestClient();
     }
 
+    @Test
     public void dummyTest() {
         assertEquals(true, true);
     }
