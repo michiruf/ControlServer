c9d327220975dca73cc745df2d373ee77559f724
Michael Ruf <Ruf_Michael@gmx.de>
2016-03-09 20:34:59 +0100

    Add initial logon server play structure

---
+++ logon-server/app/controllers/UserController.java
@@ -0,0 +1,28 @@
+package controllers;
+
+/**
+ * @author Michael Ruf
+ * @since 2016-03-09
+ */
+public class UserController extends AbstractController {
+
+    public static void login() {
+        render();
+    }
+
+    public static void doLogin(String username, String password) {
+
+    }
+
+    public static void doLoginJson() {
+
+    }
+
+    public static void register() {
+        render();
+    }
+
+    public static void doRegister(String username, String password, String mail) {
+
+    }
+}
---
+++ logon-server/app/models/User.java
@@ -0,0 +1,8 @@
+package models;
+
+/**
+ * @author Michael Ruf
+ * @since 2016-03-09
+ */
+public class User extends Model {
+}
