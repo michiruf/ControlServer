b0025dc3e16aef1e1b8c2b78b642cf43c88511fc
Michael Ruf <Ruf_Michael@gmx.de>
2016-04-07 16:17:33 +0200

    Generic entity model, spring devtools

--- logon-server/src/main/java/de/michiruf/control_server/server/model/Device.java
+++ logon-server/src/main/java/de/michiruf/control_server/server/model/Device.java
@@ -1,8 +1,15 @@
 package de.michiruf.control_server.server.model;
 
+import de.michiruf.control_server.server.helper.IdModel;
+
+import javax.persistence.ManyToOne;
+
 /**
  * @author Michael Ruf
  * @since 2016-04-07
  */
-public class Device {
+public class Device extends IdModel {
+
+    @ManyToOne
+    private User user;
 }
--- logon-server/src/main/java/de/michiruf/control_server/server/model/User.java
+++ logon-server/src/main/java/de/michiruf/control_server/server/model/User.java
@@ -1,12 +1,17 @@
 package de.michiruf.control_server.server.model;
 
+
+import de.michiruf.control_server.server.helper.IdModel;
+
+import javax.persistence.OneToMany;
 import java.util.List;
 
 /**
  * @author Michael Ruf
  * @since 2016-04-07
  */
-public class User {
+public class User extends IdModel {
 
+    @OneToMany(mappedBy = "used")
     private List<Device> devices;
 }
