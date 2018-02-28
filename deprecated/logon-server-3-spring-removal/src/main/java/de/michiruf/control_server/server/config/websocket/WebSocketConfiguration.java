f0f67012e07b7c04617d0b61dac61b21e8b561f9
Michael Ruf <Ruf_Michael@gmx.de>
2018-02-28 08:01:44 +0100

    Move spring logon-server to deprecated

--- deprecated/logon-server-3-spring-removal/src/main/java/de/michiruf/control_server/server/config/websocket/WebSocketConfiguration.java
+++ deprecated/logon-server-3-spring-removal/src/main/java/de/michiruf/control_server/server/config/websocket/WebSocketConfiguration.java
@@ -1,16 +1,11 @@
 package de.michiruf.control_server.server.config.websocket;
 
-import groovy.util.logging.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.socket.client.standard.WebSocketContainerFactoryBean;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.jetty.JettyRequestUpgradeStrategy;
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 /**
  * @author Michael Ruf
